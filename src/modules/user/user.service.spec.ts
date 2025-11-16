e2b53995c9e9c79ff795deed1616871cb1197ccb
TheCodeGhninux <ilesanmibry@gmail.com>
2025-11-17 00:52:54 +0100

    chore: fix test

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
+  const mockLogger = {
+    child: jest.fn().mockReturnThis(),
+    info: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: WINSTON_MODULE_PROVIDER,
+          useValue: mockLogger,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
