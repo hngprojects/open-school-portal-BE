8b024137d54a36cb06851dbce50ba2c96d0b251a
Praiz001 <oshilimpraisek@gmail.com>
2025-11-21 19:44:56 +0100

    test(auth): write test for  deactivate account feature

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,4 +1,8 @@
-import { HttpStatus, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpStatus,
+  NotFoundException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import * as sysMsg from '../../constants/system.messages';
@@ -12,6 +16,7 @@ describe('AuthController', () => {
 
   const mockAuthService = {
     activateUserAccount: jest.fn(),
+    deactivateUserAccount: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -76,6 +81,44 @@ describe('AuthController', () => {
       );
       await expect(controller.activateAccount(userId)).rejects.toThrow(
         sysMsg.USER_NOT_FOUND,
+      );
+    });
+  });
+
+  describe('deactivateAccount', () => {
+    it('should deactivate a user account and return a success response', async () => {
+      const userId = 'an-active-user-uuid';
+      const successResponse = { message: sysMsg.USER_DEACTIVATED };
+
+      mockAuthService.deactivateUserAccount.mockResolvedValue(successResponse);
+
+      const result = await controller.deactivateAccount(userId);
+
+      expect(authService.deactivateUserAccount).toHaveBeenCalledWith(userId);
+      expect(result).toEqual(successResponse);
+    });
+
+    it('should throw ConflictException when user is already deactivated', async () => {
+      const userId = 'a-deactivated-user-uuid';
+
+      mockAuthService.deactivateUserAccount.mockRejectedValue(
+        new ConflictException(sysMsg.USER_IS_DEACTIVATED),
+      );
+
+      await expect(controller.deactivateAccount(userId)).rejects.toThrow(
+        new ConflictException(sysMsg.USER_IS_DEACTIVATED),
+      );
+    });
+
+    it('should throw a NotFoundException if user does not exist', async () => {
+      const userId = 'a-non-existent-user-uuid';
+
+      mockAuthService.deactivateUserAccount.mockRejectedValue(
+        new NotFoundException(sysMsg.USER_NOT_FOUND),
+      );
+
+      await expect(controller.deactivateAccount(userId)).rejects.toThrow(
+        new NotFoundException(sysMsg.USER_NOT_FOUND),
       );
     });
   });
