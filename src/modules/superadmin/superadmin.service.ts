6d03717655133fa6695fe03e5db5fd1811851f36
Azeez Adebayo <debaycisse@gmail.com>
2025-11-24 14:24:05 +0100

    refactor(api): add structure for superadmin endpoint

--- src/modules/superadmin/superadmin.service.ts
+++ src/modules/superadmin/superadmin.service.ts
@@ -4,24 +4,15 @@ import { CreateSuperadminDto } from './dto/create-superadmin.dto';
 
 @Injectable()
 export class SuperadminService {
+  /**
+   * creates an instance of the superadmin - check the ./entity
+   * @param createSuperadminDto 
+   * @returns 
+   */
   createSuperAdmin(createSuperadminDto: CreateSuperadminDto) {
     /**
-     * TODO:
-     * 1. Ensures no other superadmin is available on the db with the same email AND having school name already attached to it
-     *    - If that happens, throw 403 forbidden error
-     *
-     * 2.
-     *
-     * -- Note that what is coming from the DTO is full_name.
-     * -- You must implement the enpoint to be called when
-     *    the password setup link is clicked.
+     * Implementation goes here...
      */
     return createSuperadminDto;
-  }
-
-  activateAccount() {
-    /**
-     *
-     */
   }
 }
