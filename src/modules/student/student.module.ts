f52cb7efbea7159d224fc988722ed93b9fc8b8fd
Azeez Adebayo <debaycisse@gmail.com>
2025-11-25 23:27:47 +0100

    fix: update codebase for student test

--- src/modules/student/student.module.ts
+++ src/modules/student/student.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { FileModule } from '../shared/file/file.module';
+import { UserModelAction } from '../user/model-actions/user-actions';
 import { UserModule } from '../user/user.module';
 
 import { StudentController } from './controllers';
@@ -12,7 +13,7 @@ import { StudentService } from './services';
 @Module({
   imports: [TypeOrmModule.forFeature([Student]), UserModule, FileModule],
   controllers: [StudentController],
-  providers: [StudentService, StudentModelAction],
+  providers: [StudentService, StudentModelAction, UserModelAction],
   exports: [StudentModelAction],
 })
 export class StudentModule {}
