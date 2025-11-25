3c8f04696917bf3c229b6b3b11f03e1585e1a9fb
Azeez Adebayo <debaycisse@gmail.com>
2025-11-25 23:35:51 +0100

    docs: add response dto for swagger ui

--- src/modules/superadmin/superadmin.module.ts
+++ src/modules/superadmin/superadmin.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { SessionModule } from '../session/session.module';
 
 import { SuperAdmin } from './entities/superadmin.entity';
 import { SuperadminModelAction } from './model-actions/superadmin-actions';
@@ -7,7 +11,18 @@ import { SuperadminController } from './superadmin.controller';
 import { SuperadminService } from './superadmin.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SuperAdmin])],
+  imports: [
+    TypeOrmModule.forFeature([SuperAdmin]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '120m' },
+      }),
+      inject: [ConfigService],
+    }),
+    SessionModule,
+  ],
   controllers: [SuperadminController],
   providers: [SuperadminService, SuperadminModelAction],
   exports: [SuperadminModelAction],
