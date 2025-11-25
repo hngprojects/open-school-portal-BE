6ce2e8b8622f84ea962755779094ed2569df2a1f
MyITjournal <adesitjourney@gmail.com>
2025-11-25 18:52:38 +0100

    "fix: remove unused files"

--- src/modules/academic-session/academic-session.module.ts
+++ src/modules/academic-session/academic-session.module.ts
@@ -8,14 +8,12 @@ import { AcademicSessionController } from './academic-session.controller';
 import { AcademicSessionService } from './academic-session.service';
 import { AcademicSession } from './entities/academic-session.entity';
 import { SessionClass } from './entities/session-class.entity';
-import { SessionStream } from './entities/session-stream.entity';
 import { AcademicSessionModelAction } from './model-actions/academic-session-actions';
 import { SessionClassModelAction } from './model-actions/session-class-actions';
-import { SessionStreamModelAction } from './model-actions/session-stream-actions';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([AcademicSession, SessionClass, SessionStream]),
+    TypeOrmModule.forFeature([AcademicSession, SessionClass]),
     AuthModule,
     ClassModule,
   ],
@@ -24,7 +22,6 @@ import { SessionStreamModelAction } from './model-actions/session-stream-actions
     AcademicSessionService,
     AcademicSessionModelAction,
     SessionClassModelAction,
-    SessionStreamModelAction,
   ],
   exports: [AcademicSessionService, AcademicSessionModelAction],
 })
