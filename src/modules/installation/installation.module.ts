31de341d003d7f602b835c7dada13b10d471912d
Azeez Adebayo <debaycisse@gmail.com>
2025-11-25 14:42:32 +0100

    refactor(module): handle conflict

---
+++ src/modules/installation/installation.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { InstallationController } from './installation.controller';
+import { InstallationService } from './installation.service';
+
+@Module({
+  controllers: [InstallationController],
+  providers: [InstallationService],
+})
+export class InstallationModule {}
