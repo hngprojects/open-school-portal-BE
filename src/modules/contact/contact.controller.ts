8bfa59499c2683ad25f3543b4225d0fc3e35aa9f
Elijah Arhinful <elijaharhinful8@gmail.com>
2025-11-25 07:05:21 +0000

    refactor(contact): updated swagger doc implementation

--- src/modules/contact/contact.controller.ts
+++ src/modules/contact/contact.controller.ts
@@ -1,32 +1,15 @@
-import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-
-import * as sysMsg from '../../constants/system.messages';
+import { Body, Controller, Post } from '@nestjs/common';
 
 import { ContactService } from './contact.service';
-import { ContactResponseDto } from './dto/contact-response.dto';
+import { ApiContactTags, ApiCreateContact } from './docs/contact.swagger';
 import { CreateContactDto } from './dto/create-contact.dto';
-
-@ApiTags('Contact')
 @Controller('contact')
+@ApiContactTags()
 export class ContactController {
   constructor(private readonly contactService: ContactService) {}
 
   @Post()
-  @ApiOperation({ summary: 'Submit a contact inquiry' })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: sysMsg.CONTACT_MESSAGE_SENT,
-    type: ContactResponseDto,
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: sysMsg.VALIDATION_ERROR,
-  })
-  @ApiResponse({
-    status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: sysMsg.OPERATION_FAILED,
-  })
+  @ApiCreateContact()
   create(@Body() createContactDto: CreateContactDto) {
     return this.contactService.create(createContactDto);
   }
