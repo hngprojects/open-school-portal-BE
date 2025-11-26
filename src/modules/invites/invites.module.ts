b5abd4f7498be51995917c528074f158e4a2eccd
Olalekan Macaulay <olalekan.macaulay1@gmail.com>
2025-11-26 10:12:31 -0500

    feat(invite): add invite entity, model action and module configuration

--- src/modules/invites/invites.module.ts
+++ src/modules/invites/invites.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { School } from '../school/entities/school.entity';
+import { UserModule } from '../user/user.module';
+
 import { Invite } from './entities/invites.entity';
+import { InviteModelAction } from './invite.model-action';
 import { InvitesController } from './invites.controller';
 import { InviteService } from './invites.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Invite])],
+  imports: [TypeOrmModule.forFeature([Invite, School]), UserModule],
   controllers: [InvitesController],
-  providers: [InviteService],
+  providers: [InviteService, InviteModelAction],
   exports: [InviteService],
 })
 export class InviteModule {}
