23ad211ea7bc3713b10ff1ec9addc7e70997e86e
MyITjournal <adesitjourney@gmail.com>
2025-11-28 03:09:21 +0100

    feat(BE-113 (NEW)): implement term endpoints

---
+++ src/modules/academic-term/entities/term.entity.ts
@@ -0,0 +1,49 @@
+import { Column, Entity, ManyToOne, JoinColumn } from 'typeorm';
+
+import { BaseEntity } from '../../../entities/base-entity';
+import { AcademicSession } from '../../academic-session/entities/academic-session.entity';
+
+export enum TermName {
+  FIRST = 'First term',
+  SECOND = 'Second term',
+  THIRD = 'Third term',
+}
+
+export enum TermStatus {
+  ACTIVE = 'Active',
+  ARCHIVED = 'Archived',
+}
+
+@Entity('terms')
+export class Term extends BaseEntity {
+  @Column({ name: 'session_id', type: 'uuid' })
+  sessionId: string;
+
+  @ManyToOne(() => AcademicSession, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'session_id' })
+  academicSession: AcademicSession;
+
+  @Column({
+    type: 'enum',
+    enum: TermName,
+  })
+  name: TermName;
+
+  @Column({ name: 'start_date', type: 'date' })
+  startDate: Date;
+
+  @Column({ name: 'end_date', type: 'date' })
+  endDate: Date;
+
+  @Column({
+    type: 'enum',
+    enum: TermStatus,
+    default: TermStatus.ACTIVE,
+  })
+  status: TermStatus;
+
+  @Column({ name: 'is_current', default: false })
+  isCurrent: boolean;
+}
